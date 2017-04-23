b0a8fc28c95417f4652024f972c6690d0d1a03e7
5730213008 <s5730213008@phuket.psu.ac.th>
2017-04-23 17:53:54 +0700

    template pattern

---
+++ src/gamepattern/GameDemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamepattern;
+
+/**
+ *
+ * @author 5730213008
+ */
+public class GameDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+		Tetris tetris = new Tetris();
+		tetris.startGame();
+	}
+    
+}
---
+++ src/gamepattern/gametemplate.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamepattern;
+
+/**
+ *
+ * @author 5730213008
+ */
+public interface gametemplate {
+
+    public void startGame();
+
+    public void updateGame();
+
+    public void resetGame();
+
+    public void spawnPiece();
+
+    public void rotatePiece(int newRotation);
+
+    public boolean isNewGame();
+
+    public boolean isPaused();
+
+    public boolean isGameOver();
+
+    public TileType getPieceType();
+
+    public TileType getNextPieceType();
+
+    public int getPieceCol();
+
+    public int getPieceRow();
+
+    public int getPieceRotation();
+
+    public int getLevel();
+
+    public int getScore();
+
+}
